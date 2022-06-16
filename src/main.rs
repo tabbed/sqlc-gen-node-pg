fa5a09dbbfe9aacbe6187789d7a96f034564de01
Kyle Conroy <kyle@conroy.org>
2022-06-15 21:10:31 -0700

    It works...?

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,26 @@ pub mod plugin {
     include!(concat!(env!("OUT_DIR"), "/plugin.rs"));
 }
 
-pub fn deserialize_greeter(buf: &[u8]) -> Result<plugin::CodeGenRequest, prost::DecodeError> {
+pub fn deserialize_codegen_request(buf: &[u8]) -> Result<plugin::CodeGenRequest, prost::DecodeError> {
     plugin::CodeGenRequest::decode(&mut Cursor::new(buf))
+}
+
+pub fn serialize_codegen_response(resp: &plugin::CodeGenResponse) -> Vec<u8> {
+    let mut buf = Vec::new();
+    buf.reserve(resp.encoded_len());
+
+    resp.encode(&mut buf).unwrap();
+    buf
+}
+
+pub fn create_codegen_response() -> plugin::CodeGenResponse {
+    let mut file = plugin::File::default();
+    file.name = "hello.txt".to_string();
+    file.contents = "Hello World".as_bytes().to_vec();
+
+    let mut resp = plugin::CodeGenResponse::default();
+    resp.files.push(file);
+    resp
 }
 
 fn main() -> Result<(), prost::DecodeError> {
@@ -16,10 +34,18 @@ fn main() -> Result<(), prost::DecodeError> {
     let mut stdin = stdin.lock();
     let buffer = stdin.fill_buf().unwrap();
 
-    let request_deserialized_result = match deserialize_greeter(&buffer) {
+    let _ = match deserialize_codegen_request(&buffer) {
         Ok(request_deserialized_result) => request_deserialized_result,
-        Err(e) => return Err(e),
+        Err(_e) => std::process::exit(1),
     };
-    println!("{:#?}", request_deserialized_result);
+
+    let resp = create_codegen_response();
+    let out = serialize_codegen_response(&resp);
+
+    let _ = match io::stdout().write_all(&out) {
+        Ok(result) => result,
+        Err(_e) => std::process::exit(1),
+    };
+
     Ok(())
 }
